f2871f10267af3d0921aaae13eb62137a824f2d8
R3BU5k <117865758+R3BU5k@users.noreply.github.com>
2023-08-21 13:30:17 +0200

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -29,7 +29,7 @@ self.addEventListener('fetch', event => {
     );
 });
 
-const cacheName = 'cache-v2.02'; //cache version to modify to load
+const cacheName = 'cache-v2.03'; //cache version to modify to load
 const resourcesToPrecache = [
     '/',
     'index.html',
